04b15bf95753f3b53beb8812b557358bc7220a6d
DawngTafi <104849907+DawngTafi@users.noreply.github.com>
2024-06-06 23:02:48 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,17 +4,15 @@ import cv2
 from PIL import Image
 import tempfile
 
-def train_model(data_path):
-## Train YOLO with custom datasets
-    datapath = r'data.yaml'
-# Load a model
-    model = YOLO("yolov8n.yaml")  # build a new model from YAML
-    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-    model = YOLO("yolov8n.yaml").load("yolov8n.pt")  # build from YAML and transfer weights
-# Train the model
-    results = model.train(data=datapath, epochs=10, imgsz=640)
-##Upload model after train  
-    modelpath  = r'trainmodel/weights/best.pt'
-    model = YOLO(modelpath)
 def load_model(model_path):
-    mo
+    """
+    Loads a YOLO object detection model from the specified model_path.
+
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A YOLO object detection model.
+    """
+    model = YOLO(model_path)
+    return model
